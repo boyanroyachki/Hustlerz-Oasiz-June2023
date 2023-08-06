2c23ba2216e74d3d714517850d35be23698592e3
boyanroyachki <106555994+boyanroyachki@users.noreply.github.com>
2023-08-06 16:15:46 +0300

    error pages

--- HustlerzOasiz.Web/Controllers/HomeController.cs
+++ HustlerzOasiz.Web/Controllers/HomeController.cs
@@ -45,9 +45,19 @@ namespace HustlerzOasiz.Web.Controllers
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            if (statusCode == 404 || statusCode == 400)
+            {
+                return View("Error404");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+            return View("Error");
         }
     }
 }
--- HustlerzOasiz.Web/Program.cs
+++ HustlerzOasiz.Web/Program.cs
@@ -57,8 +57,10 @@ namespace HustlerzOasiz.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error/500");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
                 app.UseHsts();
             }
 
